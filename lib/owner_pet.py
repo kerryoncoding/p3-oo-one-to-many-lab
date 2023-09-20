266417c9552aa28518a26747e2ee16a584767d73
Kerry Guarino <kerry.guarino@gmail.com>
2023-09-20 19:45:09 -0400

    stopping work on this... may not be a required lab/section

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,32 @@
 class Pet:
-    pass
+
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+
+    all_pets = []
+    
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        Pet.all.append(self)
+
+    def all(self):
+        all_pets.append(self)
+    
+    @property
+    def validate_pet_type(cls, pet_type):
+        if pet_type not in cls.PET_TYPES:
+            raise Exception("error")
+
+
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return all_pets
+    
+    def add_pet(self, pet):
+        pass
+
