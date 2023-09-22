2f800fb210ff77b6ef960019df6417a49636f8ea
Kerry Guarino <kerry.guarino@gmail.com>
2023-09-21 21:03:47 -0400

    worked lab with support from dev file

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -2,21 +2,33 @@ class Pet:
 
     PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
 
-    all_pets = []
+    all = []
     
     def __init__(self, name, pet_type, owner=None):
         self.name = name
         self.pet_type = pet_type
         self.owner = owner
         Pet.all.append(self)
-
-    def all(self):
-        all_pets.append(self)
     
     @property
-    def validate_pet_type(cls, pet_type):
-        if pet_type not in cls.PET_TYPES:
-            raise Exception("error")
+    def pet_type(self):
+        return self._pet_type
+    
+    @pet_type.setter
+    def pet_type(self, pet_type):
+        if pet_type not in self.PET_TYPES:
+            raise Exception("error - not pet type")
+        self._pet_type = pet_type
+
+    @property
+    def owner(self):
+        return self._owner
+    
+    @owner.setter
+    def owner(self,owner):
+        if not (isinstance(owner, Owner) or not owner):
+            raise Exception("error - not owner")
+        self._owner = owner
 
 
 
@@ -25,8 +37,13 @@ class Owner:
         self.name = name
 
     def pets(self):
-        return all_pets
+        return [pet for pet in Pet.all if pet.owner == self]
     
     def add_pet(self, pet):
-        pass
+        if not isinstance(pet, Pet):
+            raise Exception("input incorrect pet type")
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
 
